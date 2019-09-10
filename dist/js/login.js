e782303b092d8e31cfba024a763b6e6e92ee6d01
lssy <1580490845@qq.com>
2019-09-10 16:08:23 +0800

    1.1

---
+++ dist/js/login.js
@@ -0,0 +1,1 @@
+"use strict";$(document).ready(function(){$(".header").load("../dist/header.html"),$(".login-btn").on("click",function(){$.ajax({type:"post",url:"http://10.31.157.77/800/php/login.php",data:{user:$(".login-input1").val(),pass:$(".login-input2").val()},success:function(t){t?(alert("登录成功"),location.href="http://10.31.157.77/800/dist/"):alert("用户名和密码错误")}})})});
